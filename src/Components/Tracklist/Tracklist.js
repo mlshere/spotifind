0a4d378a56c7ac5f101144f960b0b9ab7b3e7ed7
mlshere <mlsherezade@gmail.com>
2024-06-07 14:39:35 +0200

    Add static components

---
+++ src/Components/Tracklist/Tracklist.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Track from '../Track/Track';
+import './Tracklist.css';
+
+const Tracklist = () => {
+    return (
+        <div className="TrackList">
+            <Track />
+        </div>
+    );
+}
+
+export default Tracklist;
