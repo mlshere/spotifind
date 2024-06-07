f538f2d4e3263cf75d4336d9fb96c847e2bc461c
mlshere <mlsherezade@gmail.com>
2024-06-07 17:48:30 +0200

    Implement Spotify search request

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SearchBar.css'
 
-const SearchBar = () => {
+const SearchBar = ({ onSearch }) => {
+    const [searchTerm, setSearchTerm] = useState(''); // Declare searchTerm state variable and initialize it to an empty string
+
+    const handleInputChange = (event) => {
+        setSearchTerm(event.target.value); 
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onSearch(searchTerm);
+
+    }
     return (
         <div className="SearchBar">
-            <input placeholder="Enter A Song, Album, or Artist" />
-            <button className="SearchButton">SEARCH</button>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    placeholder="Enter A Song, Album, or Artist" 
+                    type="text"
+                    value={searchTerm}
+                    onChange={handleInputChange}
+                />
+                <button type="submit" className="SearchButton">SEARCH</button>
+            </form>
         </div>
     )
 }
