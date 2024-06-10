b26ef069f7fd291db742fd3464b59295effc2813
mlshere <mlsherezade@gmail.com>
2024-06-10 11:48:09 +0200

    Implement Playlist. Add, remove and rename

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Playlist.css';
 import Tracklist from '../Tracklist/Tracklist';
 
-const Playlist = () => {
+const Playlist = ({ playlistTracks, onRemove, onNameChange, onSave}) => {
+    const [playlistName, setPlaylistName] = useState('New Playlist')
+    
+    const handleNameChange = (e) => {
+        setPlaylistName(e.target.value);
+        onNameChange(e.target.value);
+    };
+    
     return (
         <div className="Playlist">
            <h2>My Playlist</h2>
+           <input 
+                value={playlistName}
+                onChange={handleNameChange} />
            <div className="TrackList">
-                <Tracklist />
+                <Tracklist tracks={playlistTracks} onRemove={onRemove} isRemoval={true}/>
            </div>
-            <button className="SavePlaylistButton">SAVE TO SPOTIFY</button>
+            <button className="SavePlaylistButton" onClick={onSave}>SAVE TO SPOTIFY</button>
         </div>
             
     );
-}
+};
 
 export default Playlist;
