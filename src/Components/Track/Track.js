15fab0fc492c39725f7f1d03909cfdf34419491f
mlshere <mlsherezade@gmail.com>
2024-06-10 11:47:05 +0200

    Implement Track functionality

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import './Track.css';
 
-const Track = () => {
+const Track = ({ track, onAdd, onRemove, isRemoval }) => {
+    const addTrack = () => {
+        onAdd(track);
+    };
+
+    const removeTrack = () => {
+        onRemove(track);
+    };
+
     return (
         <div className="Track">
             <div className="Track-information">
                 <h3>{/* Track name */}</h3>
                 <p>{/* Artist */} | {/* Album */}</p>
             </div>
-            <button className="Track-action">{/* + or - button */}</button>
+            {isRemoval ? (
+                <button className="Track-action" onClick={removeTrack}>-</button>
+            ) : (
+                <button className="Track-action" onClick={addTrack}>+</button>
+            )}    
         </div>
     );
 }
