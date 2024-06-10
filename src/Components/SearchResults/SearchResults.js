67b5b500ad81b854ace7602ce2004656ed419a22
mlshere <mlsherezade@gmail.com>
2024-06-10 10:18:48 +0200

    Implement search results

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -3,14 +3,20 @@ import Track from '../Track/Track';
 import './SearchResults.css';
 
 
-const SearchResults = () => {
+const SearchResults = ( {tracks} ) => {
     return (
         <div className="SearchResults">
             <h2>SEARCH RESULTS</h2>
-            <div className="TrackList">
-                <Track />
-        
-            </div>
+            <ul className="TrackList">
+                {tracks.map(track => (
+                    <li key={track.id}>
+                        <div>
+                            <strong>{track.name}</strong>
+                            <p>{track.artist} | {track.album}</p>
+                        </div>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
