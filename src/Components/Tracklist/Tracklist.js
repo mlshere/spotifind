94368b4c2f04493bec3024e2a071256659d12f84
mlshere <mlsherezade@gmail.com>
2024-06-10 11:46:29 +0200

    Implement Tracklist functionality

--- src/Components/Tracklist/Tracklist.js
+++ src/Components/Tracklist/Tracklist.js
@@ -2,12 +2,20 @@ import React from 'react';
 import Track from '../Track/Track';
 import './Tracklist.css';
 
-const Tracklist = () => {
+const Tracklist = ({ tracks, onAdd, onRemove, isRemoval }) => {
     return (
-        <div className="TrackList">
-            <Track />
+        <div className="TrackList">{
+            tracks.map(track => (
+                <Track 
+                    key={track.id} 
+                    track={track} 
+                    onAdd={onAdd} 
+                    onRemove={onRemove} 
+                    isRemoval={isRemoval} 
+                />
+            ))}       
         </div>
     );
-}
+};
 
 export default Tracklist;
