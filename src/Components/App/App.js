25a1f777fedd0d95a84a8361a6372e1b2f788923
mlshere <mlsherezade@gmail.com>
2024-06-10 15:39:01 +0200

    Update name to Spotifind

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -46,7 +46,7 @@ const App = () => {
   return (
     <div>
       <h1>
-        Ja<span className="highlight">mmm</span>ing
+        Spoti<span className="highlight">find</span>
       </h1>
       <div className="App">
         <SearchBar onSearch={search} />
